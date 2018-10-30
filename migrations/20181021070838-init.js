c6999b540fda3fcd09e4397d6cf83bfba4761742
Tyler Collier <git@tylercollier.com>
2018-10-29 21:38:29 -0700

    #4 Add pledges table

--- migrations/20181021070838-init.js
+++ migrations/20181021070838-init.js
@@ -1,62 +1,70 @@
-'use strict';
+'use strict'
 
-var dbm;
-var type;
-var seed;
+var dbm
+var type
+var seed
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-};
+  dbm = options.dbmigrate
+  type = dbm.dataType
+  seed = seedLink
+}
 
 exports.up = function(db) {
-  return db.createTable('users', {
-    id: { type: 'int', primaryKey: true, autoIncrement: true },
-    first_name: { type: 'string', length: 50 },
-    last_name: { type: 'string', length: 50 },
-  })
-    .then(() => db.createTable('groups', {
+  return db
+    .createTable('users', {
       id: { type: 'int', primaryKey: true, autoIncrement: true },
-      name: { type: 'string', length: 50 },
-    }))
-    .then(() => db.createTable('memberships', {
-      id: { type: 'int', primaryKey: true, autoIncrement: true },
-      user_id: {
-        type: 'int',
-        notNull: true,
-        foreignKey: {
-          name: 'memberships_user_id_fk',
-          table: 'users',
-          rules: {
-            onDelete: 'CASCADE',
+      first_name: { type: 'string', length: 50 },
+      last_name: { type: 'string', length: 50 },
+    })
+    .then(() =>
+      db.createTable('groups', {
+        id: { type: 'int', primaryKey: true, autoIncrement: true },
+        name: { type: 'string', length: 50 },
+      })
+    )
+    .then(() =>
+      db.createTable('memberships', {
+        id: { type: 'int', primaryKey: true, autoIncrement: true },
+        user_id: {
+          type: 'int',
+          notNull: true,
+          foreignKey: {
+            name: 'memberships_user_id_fk',
+            table: 'users',
+            rules: {
+              onDelete: 'CASCADE',
+            },
+            mapping: 'id',
           },
-          mapping: 'id'
         },
-      },
-      group_id: {
-        name: 'memberships_group_id_fk',
-        type: 'int',
-        notNull: true,
-        foreignKey: {
-          table: 'groups',
-          rules: {
-            onDelete: 'CASCADE',
+        group_id: {
+          name: 'memberships_group_id_fk',
+          type: 'int',
+          notNull: true,
+          foreignKey: {
+            table: 'groups',
+            rules: {
+              onDelete: 'CASCADE',
+            },
+            mapping: 'id',
           },
-          mapping: 'id'
         },
-      },
-    }));
-};
+      })
+    )
+}
 
 exports.down = function(db) {
-  return db.dropTable('users');
-};
+  return db
+    .dropTable('memberships')
+    .then(() => db.dropTable('users'))
+    .then(() => db.dropTable('groups'))
+}
 
 exports._meta = {
-  "version": 1
-};
+  version: 1,
+}
