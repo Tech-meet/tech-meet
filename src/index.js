28e6bc839c7e1391d1e11829798091e9f7dec692
Tyler Collier <git@tylercollier.com>
2018-10-29 22:28:30 -0700

    #5 Add hot reloading

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
+import { AppContainer } from 'react-hot-loader'
 
 const client = new ApolloClient()
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-)
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+render()
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render()
+  })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
